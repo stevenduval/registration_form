981a583cf21c06233e30ec805ab5701272dfdb6e
stevenduval <stevenduval@gmail.com>
2021-09-24 17:09:15 -0500

    test data

---
+++ data.js
@@ -0,0 +1,44 @@
+{
+   "form": [
+      {
+         "fields": [
+            {
+               "label": "First Name",
+               "name": "FIRST_NAME",
+               "id": "FIRST_NAME",
+               "type":"input"
+            },
+            {
+               "label": "Last Name",
+               "name": "LAST_NAME",
+               "id": "LAST_NAME",
+               "type":"input"
+            },
+            {
+               "label": "Email",
+               "name": "EMAIL",
+               "id": "EMAIL",
+               "type":"input"
+            },
+            {
+               "label": "Address",
+               "name": "ADDRESS",
+               "id": "ADDRESS",
+               "type":"input"
+            },
+            {
+               "label": "Year of Graduation",
+               "name": "GRADUATION_YEAR",
+               "id": "GRADUATION_YEAR",
+               "type":"input"
+            },
+            {
+               "label": "Specialty",
+               "name": "SPECIALTY",
+               "id": "SPECIALTY",
+               "type":"input"
+            }
+         ]
+      }
+   ]
+}
