083ee8fc6f4c00d2a32298bbf8a15559d45517b7
stevenduval <stevenduval@gmail.com>
2021-09-27 13:25:34 -0500

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,45 @@
+{
+   "form": [
+      {
+         "fields": [
+            {
+               "label": "First Name",
+               "name": "FIRST_NAME",
+               "id": "FIRST_NAME",
+               "type":"input"
+            },
+            {
+               "label": "Last Name",
+               "name": "LAST_NAME",
+               "id": "LAST_NAME",
+               "type":"input"
+            },
+            {
+               "label": "Email Address",
+               "name": "EMAIL",
+               "id": "EMAIL",
+               "type":"input"
+            },
+            {
+               "label": "Postal Address",
+               "name": "ADDRESS",
+               "id": "ADDRESS",
+               "type":"input"
+            },
+            {
+               "label": "Year of Graduation",
+               "name": "GRADUATION_YEAR",
+               "id": "GRADUATION_YEAR",
+               "type":"input"
+            },
+            {
+               "label": "Specialty",
+               "name": "SPECIALTY",
+               "id": "SPECIALTY",
+               "type": "select",
+               "options": ["Allergy & Immunology","Anesthesiology","Cardiology","Critical Care Medicine","Dermatology","Diagnostic Radiology","Emergency Medicine","Endocrinology Diabetes and Metabolism","Gastroenterology","Hematology","Infectious Disease","Internal Medicine","Medical Genetics","Nephrology","Neurology","Nuclear Medicine","Obstetrics & Gynecology","Oncology","Ophthalmology","Orthopedics","Otolaryngology","Pain Management","Pathology","Pediatrics","Physical Medicine & Rehabilitation","Plastic Surgery/Aesthetics","Preventive Medicine","Psychiatry","Pulmonology","Radiology","Rheumatology","Sleep Medicine","Sports Medicine","Surgery","Urology","Other"] 
+            }
+         ]
+      }
+   ]
+}
