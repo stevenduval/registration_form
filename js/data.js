9320151363602d43c0d0de9b26ab9ca05899798e
stevenduval <stevenduval@gmail.com>
2021-09-28 09:19:11 -0500

    Update data.js

--- js/data.js
+++ js/data.js
@@ -6,49 +6,50 @@
                "label": "First Name",
                "name": "FIRST_NAME",
                "id": "FIRST_NAME",
-               "type":"input"
+               "type": "input"
             },
             {
                "label": "Last Name",
                "name": "LAST_NAME",
                "id": "LAST_NAME",
-               "type":"input"
+               "type": "input"
             },
             {
                "label": "Email Address",
                "name": "EMAIL",
                "id": "EMAIL",
-               "type":"input"
+               "type": "input"
             },
             {
                "label": "Street Address",
                "name": "STREET_ADDRESS",
                "id": "STREET_ADDRESS",
-               "type":"input"
+               "type": "input"
             },
             {
                "label": "City",
                "name": "CITY",
                "id": "CITY",
-               "type":"input"
+               "type": "input"
             },
             {
                "label": "State",
                "name": "STATE",
                "id": "STATE",
-               "type":"input"
+               "type": "select",
+               "options": ["Alabama","Alaska","American Samoa","Arizona","Arkansas","California","Colorado","Connecticut","Delaware","District of Columbia","Federated States of Micronesia","Florida","Georgia","Guam","Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Marshall Islands","Maryland","Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana","Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York","North Carolina","North Dakota","Northern Mariana Islands","Ohio","Oklahoma","Oregon","Palau","Pennsylvania","Puerto Rico","Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah","Vermont","Virgin Island","Virginia","Washington","West Virginia","Wisconsin","Wyoming"] 
             },
             {
                "label": "Zip Code",
                "name": "ZIP_CODE",
                "id": "ZIP_CODE",
-               "type":"input"
+               "type": "input"
             },
             {
                "label": "Year of Graduation",
                "name": "GRADUATION_YEAR",
                "id": "GRADUATION_YEAR",
-               "type":"input"
+               "type": "input"
             },
             {
                "label": "Specialty",
