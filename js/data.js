a4495ee255d42d3f4f3a71b211263442dc565832
stevenduval <stevenduval@gmail.com>
2021-09-28 09:06:19 -0500

    Update data.js

--- js/data.js
+++ js/data.js
@@ -11,19 +11,6 @@
             {
                "label": "Last Name",
                "name": "LAST_NAME",
-               "id": "LAST_NAME",{
-   "form": [
-      {
-         "fields": [
-            {
-               "label": "First Name",
-               "name": "FIRST_NAME",
-               "id": "FIRST_NAME",
-               "type":"input"
-            },
-            {
-               "label": "Last Name",
-               "name": "LAST_NAME",
                "id": "LAST_NAME",
                "type":"input"
             },
@@ -44,64 +31,6 @@
                "name": "CITY",
                "id": "CITY",
                "type":"input"
-            },
-//            {
-//               "label": "State",
-//               "name": "STATE",
-//               "id": "STATE",
-//               "type": "select",
-//               "options": ["Alabama","Alaska","American Samoa","Arizona","Arkansas","California","Colorado","Connecticut","Delaware","District of Columbia","Federated States of Micronesia","Florida","Georgia","Guam","Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Marshall Islands","Maryland","Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana","Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York","North Carolina","North Dakota","Northern Mariana Islands","Ohio","Oklahoma","Oregon","Palau","Pennsylvania","Puerto Rico","Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah","Vermont","Virgin Island","Virginia","Washington","West Virginia","Wisconsin","Wyoming"]
-//            },
-            {
-               "label": "Zip Code",
-               "name": "ZIP_CODE",
-               "id": "ZIP_CODE",
-               "type":"input"
-            },
-            {
-               "label": "Year of Graduation",
-               "name": "GRADUATION_YEAR",
-               "id": "GRADUATION_YEAR",
-               "type":"input"
-            },
-            {
-               "label": "Specialty",
-               "name": "SPECIALTY",
-               "id": "SPECIALTY",
-               "type": "select",
-               "options": ["Allergy & Immunology","Anesthesiology","Cardiology","Critical Care Medicine","Dermatology","Diagnostic Radiology","Emergency Medicine","Endocrinology Diabetes and Metabolism","Gastroenterology","Hematology","Infectious Disease","Internal Medicine","Medical Genetics","Nephrology","Neurology","Nuclear Medicine","Obstetrics & Gynecology","Oncology","Ophthalmology","Orthopedics","Otolaryngology","Pain Management","Pathology","Pediatrics","Physical Medicine & Rehabilitation","Plastic Surgery/Aesthetics","Preventive Medicine","Psychiatry","Pulmonology","Radiology","Rheumatology","Sleep Medicine","Sports Medicine","Surgery","Urology","Other"] 
-            }
-         ]
-      }
-   ]
-}
-
-               "type":"input"
-            },
-            {
-               "label": "Email Address",
-               "name": "EMAIL",
-               "id": "EMAIL",
-               "type":"input"
-            },
-            {
-               "label": "Street Address",
-               "name": "STREET_ADDRESS",
-               "id": "STREET_ADDRESS",
-               "type":"input"
-            },
-            {
-               "label": "City",
-               "name": "CITY",
-               "id": "CITY",
-               "type":"input"
-            },
-            {
-               "label": "State",
-               "name": "STATE",
-               "id": "STATE",
-               "type": "select",
-               "options": ["Alabama","Alaska","American Samoa","Arizona","Arkansas","California","Colorado","Connecticut","Delaware","District of Columbia","Federated States of Micronesia","Florida","Georgia","Guam","Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Marshall Islands","Maryland","Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana","Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York","North Carolina","North Dakota","Northern Mariana Islands","Ohio","Oklahoma","Oregon","Palau","Pennsylvania","Puerto Rico","Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah","Vermont","Virgin Island","Virginia","Washington","West Virginia","Wisconsin","Wyoming"]
             },
             {
                "label": "Zip Code",
