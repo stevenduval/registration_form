e7b415a6a00082d9d463a7f275dba6739e6c0952
stevenduval <stevenduval@gmail.com>
2021-10-19 12:44:00 -0500

    update to use data list

--- js/data.js
+++ js/data.js
@@ -33,10 +33,11 @@
                "type": "input"
             },
             {
-               "label": "State",
+               "label": "State/Territory",
                "name": "STATE",
                "id": "STATE",
                "type": "select",
+               "list" : "states",
                "options": ["Alabama","Alaska","American Samoa","Arizona","Arkansas","California","Colorado","Connecticut","Delaware","District of Columbia","Federated States of Micronesia","Florida","Georgia","Guam","Hawaii","Idaho","Illinois","Indiana","Iowa","Kansas","Kentucky","Louisiana","Maine","Marshall Islands","Maryland","Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana","Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York","North Carolina","North Dakota","Northern Mariana Islands","Ohio","Oklahoma","Oregon","Palau","Pennsylvania","Puerto Rico","Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah","Vermont","Virgin Island","Virginia","Washington","West Virginia","Wisconsin","Wyoming"] 
             },
             {
@@ -56,6 +57,7 @@
                "name": "SPECIALTY",
                "id": "SPECIALTY",
                "type": "select",
+               "list" : "specialties",
                "options": ["Allergy & Immunology","Anesthesiology","Cardiology","Critical Care Medicine","Dermatology","Diagnostic Radiology","Emergency Medicine","Endocrinology Diabetes and Metabolism","Gastroenterology","Hematology","Infectious Disease","Internal Medicine","Medical Genetics","Nephrology","Neurology","Nuclear Medicine","Obstetrics & Gynecology","Oncology","Ophthalmology","Orthopedics","Otolaryngology","Pain Management","Pathology","Pediatrics","Physical Medicine & Rehabilitation","Plastic Surgery/Aesthetics","Preventive Medicine","Psychiatry","Pulmonology","Radiology","Rheumatology","Sleep Medicine","Sports Medicine","Surgery","Urology","Other"] 
             }
          ]
